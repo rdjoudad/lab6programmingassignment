98370d55321a35bbe73b3b48168ec2247f705171
Ryma Djoudad <zenyod85@gmail.com>
2026-02-21 14:25:24 -0500

    Trying to structure code

--- Lab6_rdjoudad-2.py
+++ Lab6_rdjoudad-2.py
@@ -6,3 +6,13 @@ No starter code
 2/21/2026
 """
 
+grocery_items = {}
+
+print("Welcome to the store!")
+
+while input != "checkout":
+    item = input("Enter an item, or 'view' to see your cart, or 'checkout' to finish.")
+    if input == "view":
+        for key, value in grocery_items:
+            print(key, value)
+    
